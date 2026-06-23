47b9261992006a21679425d78d1141ed7e52f8ed
Kimoto1336 <yegor.lopatin.07@bk.ru>
2026-06-23 19:44:19 +0300

    proverka

---
+++ prisma/seed.js
@@ -0,0 +1,86 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+    await prisma.fact.createMany({
+
+
+        data: [
+
+            {
+                title: "Бананы являются ягодами",
+                category: "Наука",
+                truth: true,
+                explanation:
+                    "С точки зрения ботаники банан относится к ягодам."
+            },
+
+            {
+                title: "Китайскую стену видно из космоса",
+                category: "История",
+                truth: false,
+                explanation:
+                    "Это миф. Невооружённым глазом её почти невозможно увидеть."
+            },
+
+            {
+                title: "У осьминога три сердца",
+                category: "Животные",
+                truth: true,
+                explanation:
+                    "Два сердца качают кровь к жабрам, одно — к остальному телу."
+            },
+
+            {
+                title: "Мёд никогда не портится",
+                category: "Наука",
+                truth: true,
+                explanation:
+                    "Благодаря составу мёд может храниться тысячи лет."
+            },
+
+            {
+                title: "Земля плоская",
+                category: "География",
+                truth: false,
+                explanation:
+                    "Земля имеет форму геоида и близка к шару."
+            },
+
+            {
+                title: "Акулы существовали раньше деревьев",
+                category: "Животные",
+                truth: true,
+                explanation:
+                    "Первые акулы появились примерно 450 млн лет назад, деревья — около 390 млн."
+            },
+
+            {
+                title: "Пингвины живут только в Антарктиде",
+                category: "Животные",
+                truth: false,
+                explanation:
+                    "Пингвины обитают также в Южной Америке, Африке, Австралии и Новой Зеландии."
+            },
+
+            {
+                title: "Солнце белого цвета",
+                category: "Космос",
+                truth: true,
+                explanation:
+                    "Из космоса Солнце выглядит белым, а жёлтым кажется из-за атмосферы Земли."
+            }
+
+        ]
+
+
+    });
+
+    console.log("Факты успешно добавлены");
+}
+
+main()
+    .catch(console.error)
+    .finally(() => prisma.$disconnect());
